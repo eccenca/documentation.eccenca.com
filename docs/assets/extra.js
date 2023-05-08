ff18263c41bffaf63e051aae8d025f7980dc7b00
anda2002 <anda.spanu@gmail.com>
2023-05-08 12:26:36 +0300

    hide header for urls that include sandbox

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -1,2 +1,18 @@
 /* site specific tweaks */
-alert("hello")
+var header = document.querySelector("header");
+let previousUrl = "";
+
+// hide header for urls that include 'sandbox'
+const observer = new MutationObserver(() => {
+  if (location.href !== previousUrl) {
+    previousUrl = location.href;
+    if (location.href?.includes("sandbox")) {
+      header.style.visibility = "hidden";
+    } else {
+      header.style.visibility = "visible";
+    }
+  }
+});
+
+const config = { subtree: true, childList: true };
+observer.observe(document, config);
