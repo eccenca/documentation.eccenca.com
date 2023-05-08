dfcf472def6aba2039d77d0cd86e73da05cfacfd
Sebastian Tramp <sebastian.tramp@eccenca.com>
2023-05-08 11:42:00 +0200

    hide also the footer

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -1,18 +1,23 @@
 /* site specific tweaks */
+
+// hide header and footer for urls that include 'with-your-sandbox'
 var header = document.querySelector("header");
+var footer = document.querySelector("footer");
 let previousUrl = "";
 
-// hide header for urls that include 'sandbox'
 const observer = new MutationObserver(() => {
   if (location.href !== previousUrl) {
     previousUrl = location.href;
-    if (location.href?.includes("sandbox")) {
-      header.style.visibility = "hidden";
+    if (location.href?.includes("with-your-sandbox")) {
+      header.style.display = "none";
+      footer.style.display = "none";
     } else {
-      header.style.visibility = "visible";
+      header.style.display = "block";
+      footer.style.display = "block";
     }
   }
 });
 
 const config = { subtree: true, childList: true };
 observer.observe(document, config);
+
